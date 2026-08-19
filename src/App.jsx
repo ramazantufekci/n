6505a5e3e77194b86ee3b56a6c5734ba6d2c0737
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-08-19 16:15:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import React, { useEffect, useState } from 'react';
 function App(){
+  const [polls, setPolls] = useState([]);
+  const submit = (e)=>{
+    console.log(e);
+  }
   return(
     <>
-      selam
+      <from>
+        <input name="is" />
+        <button onChange={submit}>Gönder</button>button>
+      </from>
     </>
   );
 }
