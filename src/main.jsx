3e26416b8ad29b5f58c32a0a41b758585203b122
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-08-19 15:35:59 +0300

    Initialize main.jsx with React application setup

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createRoot,ReactDOM  } from 'react-dom/client'
+import App from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+      <App />
+   
+    
+  </React.StrictMode>,
+)
