22abf3d0d707d77d89679eb3a9755f884af30906
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-08-19 16:26:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 function App(){
-  const [polls, setPolls] = useState([]);
+  const [polls, setPolls] = useState('');
   const submit = (e)=>{
     console.log(e);
   }
   return(
     <>
       <from>
-        <input name="is" />
-        <button onChange={submit}>Gönder</button>button>
+        <input name="is" value={polls} onChange={(e)=>setPolls(e.target.value)} />
+        <button onClick={submit}>Gönder</button>
       </from>
     </>
   );
